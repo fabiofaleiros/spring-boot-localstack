59633ad8eb39a6add853046bf063aefe40374c96
Fábio Siqueira <fabio.siqueira@axians.com>
2024-04-23 19:23:31 +0100

    feat: initial commit

---
+++ src/test/java/pt/ffs/springlocalstack/springbootlocalstack/SpringBootLocalstackApplicationTests.java
@@ -0,0 +1,13 @@
+package pt.ffs.springlocalstack.springbootlocalstack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootLocalstackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
